91d7539df32eb0b3b13ddaf16e2f3919ce93f9ef
vpp090 <vasil.petrov090@gmail.com>
2023-12-02 15:28:04 +0200

    shopping aggregator service added.

---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class ShoppingModel
+    {
+        public string Username { get; set; }
+        public BasketModel BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+       
+    }
+}
