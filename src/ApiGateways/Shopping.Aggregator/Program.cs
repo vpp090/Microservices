3f83d787da80bc17cfbfeae369d12741ec99c854
vpp090 <vasil.petrov090@gmail.com>
2023-12-02 16:46:00 +0200

    Services to the original microservices created extension methods created.

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,9 +1,20 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<ICatalogService,  CatalogService>(c => 
+  c.BaseAddress = new Uri(builder.Configuration["ApplicationSettings:CatalogUrl"]));
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApplicationSettings:BasketUrl"]));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApplicationSettings:OrderUrl"]));
 
 var app = builder.Build();
 
