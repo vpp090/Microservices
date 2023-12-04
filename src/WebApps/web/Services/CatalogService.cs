dcffd53e7c7cdf5e42c136b477a6f653ed10201e
vpp090 <vasil.petrov090@gmail.com>
2023-12-04 16:51:35 +0200

    CatalogService updated.

--- src/WebApps/web/Services/CatalogService.cs
+++ src/WebApps/web/Services/CatalogService.cs
@@ -1,5 +1,7 @@
 ﻿using AspnetRunBasics.Contracts;
+using AspnetRunBasics.Extensions;
 using AspnetRunBasics.Models;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -14,24 +16,45 @@ namespace AspnetRunBasics.Services
         {
             _client = client;
         }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            var response = await _client.GetAsync("/Catalog");
+            var result = await response.ReadContentAs<List<CatalogModel>>();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var response = await _client.GetAsync($"/Catalog/GetProductByCategory/{category}");
+            var result = await response.ReadContentAs<List<CatalogModel>>();
+
+            return result;
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            var response = await _client.GetAsync($"/Catalog/{id}");
+            var result = await response.ReadContentAs<CatalogModel>();
+
+            return result;
+        }
+
         public async Task<CatalogModel> CreateCatalog(CatalogModel model)
         {
-            throw new System.NotImplementedException();
-        }
+            CatalogModel result;
+            var response = await _client.PostAsJson($"/Catalog", model);
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.ReadContentAs<CatalogModel>();
+                return result;
+            }
+            else
+            {
+                throw new System.Exception("Something went wrong");
+            }
 
-        public Task<IEnumerable<CatalogModel>> GetCatalog()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<CatalogModel> GetCatalog(string id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
-        {
-            throw new System.NotImplementedException();
-        }
+        }       
     }
 }
