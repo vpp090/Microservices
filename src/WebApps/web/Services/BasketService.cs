0834558189f28fb58b1fbd89360721ff34146b11
vpp090 <vasil.petrov090@gmail.com>
2023-12-05 17:24:51 +0200

    BasketService completed

--- src/WebApps/web/Services/BasketService.cs
+++ src/WebApps/web/Services/BasketService.cs
@@ -1,24 +1,43 @@
 ﻿using AspnetRunBasics.Contracts;
+using AspnetRunBasics.Extensions;
 using AspnetRunBasics.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class BasketService : IBasketService
     {
-        public Task CheckoutBasket(BasketCheckoutModel model)
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
         {
-            throw new System.NotImplementedException();
+            _client = client;
         }
 
-        public Task<BasketModel> GetBasket(string userName)
+        public async Task<BasketModel> GetBasket(string userName)
         {
-            throw new System.NotImplementedException();
+            var response = await _client.GetAsync($"/Basket/{userName}");
+            var result = await response.ReadContentAs<BasketModel>();
+
+            return result;
         }
 
-        public Task<BasketModel> UpdateBasket(BasketModel model)
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
         {
-            throw new System.NotImplementedException();
+            var response = await _client.PostAsJson($"/Basket", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<BasketModel>();
+            else
+                throw new System.Exception("Something went wrong when calling api");
+        }
+
+        public async Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            var response = await _client.PostAsJson($"/Basket/Checkout", model);
+            if (!response.IsSuccessStatusCode)
+                throw new System.Exception("Something went wrong when calling api");
         }
     }
 }
