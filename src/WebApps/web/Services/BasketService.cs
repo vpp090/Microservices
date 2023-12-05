8a89d01c5240b580438ae9b29b5cc374e570f37b
vpp090 <vasil.petrov090@gmail.com>
2023-12-05 17:17:39 +0200

    basic implementation of basketservice

--- src/WebApps/web/Services/BasketService.cs
+++ src/WebApps/web/Services/BasketService.cs
@@ -1,8 +1,24 @@
 ﻿using AspnetRunBasics.Contracts;
+using AspnetRunBasics.Models;
+using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class BasketService : IBasketService
     {
+        public Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketModel> GetBasket(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
