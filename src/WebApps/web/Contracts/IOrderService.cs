1df9c0fa4f278cf3acf21a85517e991cd13dadc1
vpp090 <vasil.petrov090@gmail.com>
2023-12-05 19:32:19 +0200

    OrderService completed

--- src/WebApps/web/Contracts/IOrderService.cs
+++ src/WebApps/web/Contracts/IOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace AspnetRunBasics.Contracts
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspnetRunBasics.Models;
+
+namespace AspnetRunBasics.Contracts
 {
     public interface IOrderService
     {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+
     }
 }
--- src/WebApps/web/Services/OrderService.cs
+++ src/WebApps/web/Services/OrderService.cs
@@ -1,8 +1,26 @@
 ﻿using AspnetRunBasics.Contracts;
+using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class OrderService : IOrderService
     {
+        private HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/Order/{userName}");
+
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
     }
 }
