afdad1c9e43799ca1474e86717a4acad5e024445
vpp090 <vasil.petrov090@gmail.com>
2023-12-05 21:00:49 +0200

    Cart page finished

--- src/WebApps/web/Pages/Cart.cshtml.cs
+++ src/WebApps/web/Pages/Cart.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
-
+using AspnetRunBasics.Contracts;
+using AspnetRunBasics.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,25 +10,35 @@ namespace AspnetRunBasics
 {
     public class CartModel : PageModel
     {
-        
+        private readonly IBasketService _basketService;
+        private readonly ICatalogService _catalogService;
 
-        public CartModel()
+        public CartModel(IBasketService basketService, ICatalogService catalogService)
         {
-           
+            _catalogService = catalogService;
+            _basketService = basketService;
         }
 
-       
+        public BasketModel Cart { get; set; } = new BasketModel();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            
+            var userName = "vv";
+            Cart = await _basketService.GetBasket(userName);
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostRemoveToCartAsync(int cartId, int cartItemId)
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
         {
-            
+            var userName = "vv";
+            var basket = await _basketService.GetBasket(userName);
+
+            var item = basket.Items.Single(x => x.ProductId == productId);
+            basket.Items.Remove(item);
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+
             return RedirectToPage();
         }
     }
