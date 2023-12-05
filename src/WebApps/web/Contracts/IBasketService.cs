e6f226331187a8e35fb162c4708f8251be305185
vpp090 <vasil.petrov090@gmail.com>
2023-12-05 17:17:01 +0200

    IBasketService contract

--- src/WebApps/web/Contracts/IBasketService.cs
+++ src/WebApps/web/Contracts/IBasketService.cs
@@ -1,6 +1,12 @@
-﻿namespace AspnetRunBasics.Contracts
+﻿using AspnetRunBasics.Models;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Contracts
 {
     public interface IBasketService
     {
+        Task CheckoutBasket(BasketCheckoutModel model);
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> UpdateBasket(BasketModel model);
     }
 }
