f9d15a0bafcf7df12aa9696711c1766c77e17980
vpp090 <vasil.petrov090@gmail.com>
2023-12-05 20:07:18 +0200

    Product Page completed.

--- src/WebApps/web/Pages/Product.cshtml.cs
+++ src/WebApps/web/Pages/Product.cshtml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using AspnetRunBasics.Contracts;
+using AspnetRunBasics.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,26 +12,59 @@ namespace AspnetRunBasics
 {
     public class ProductModel : PageModel
     {
-     
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
 
-        public ProductModel()
+        public IEnumerable<string> CategoryList { get; set; } = new List<string>();
+        public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
+
+        public ProductModel(ICatalogService catalogService, IBasketService basketService)
         {
-         
+            _catalogService = catalogService;
+            _basketService = basketService;
         }
-
-     
 
         [BindProperty(SupportsGet = true)]
         public string SelectedCategory { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? categoryId)
+
+        public async Task<IActionResult> OnGetAsync(string categoryName)
         {
+            var productList = await _catalogService.GetCatalog();
+            CategoryList = productList.Select(p => p.Category).Distinct();
+
+            if(!string.IsNullOrWhiteSpace(categoryName))
+            {
+                ProductList = productList.Where(p => p.Category == categoryName);
+                SelectedCategory = categoryName;
+            }
+            else
+            {
+                ProductList = productList;
+            }
+
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
         {
-            return Page();
+            var product = await _catalogService.GetCatalog(productId);
+
+            var userName = "vv";
+            var basket = await _basketService.GetBasket(userName);
+
+            basket.Items.Add(new BasketItemModel
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 1,
+                Color = (int)Color.Yellow
+            });
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+
+            return RedirectToPage();
         }
     }
 }
